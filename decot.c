6d107e802159b2638d38a9c76e34194a1a3f0c8f
Peter Szabo <pts@fazekas.hu>
2018-01-02 03:28:23 +0100

    updated upxbc

--- decot.c
+++ decot.c
@@ -9,24 +9,13 @@
 int main(int argc, char **argv) {
   char *outp;
   (void)argc; (void)argv;
-  outp = malloc(sz_unc + 4);  /* !! why +4? fast? */
+  outp = malloc(sz_unc + 4);
   memcpy(outp + sz_unc, "DCBA", 4);
   fprintf(stderr, "outs0=%d outp=%p\n", sz_unc, outp);
-#if 0  /* Works. */
-  {
-    /* 0: OK, -1 (etc.): error. */
-    int got;
-    got = decompress(compressed_data, sz_cpr, outp, &sz_unc /* , 0 b_method */);
-    /* decompress doesn't seem tho change *sz_unc. */
-    fprintf(stderr, "got=%d sz_unc=%d canary_ok=%d\n", got, sz_unc, 0 == memcmp(outp + sz_unc, "DCBA", 4));
-    if (b_ftid != 0) {
-      lxunfilter(outp, sz_unc, b_cto8 /* , 0 b_ftid */);
-    }
-  }
-#else  /* Works. */
   smart_decompress(outp);
-#endif
   fprintf(stderr, "canary_ok2=%d\n", 0 == memcmp(outp + sz_unc, "DCBA", 4));
-  write(1, outp, sz_unc);
+  /* uncompressed_data_size <= sz_unc. There is padding afterwards. */
+  write(1, before_signature_data, AFTER_SIGNATURE_OFS);
+  write(1, outp, uncompressed_data_size - AFTER_SIGNATURE_OFS);
   return 0;
 }
