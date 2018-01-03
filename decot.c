f59fbbbe4a88cd582017df32b66a6e1073aad02c
Peter Szabo <pts@fazekas.hu>
2018-01-03 02:23:25 +0100

    updated upxbc

--- decot.c
+++ decot.c
@@ -9,13 +9,16 @@
 int main(int argc, char **argv) {
   char *outp;
   (void)argc; (void)argv;
-  outp = malloc(sz_unc + 4);
-  memcpy(outp + sz_unc, "DCBA", 4);
-  fprintf(stderr, "outs0=%d outp=%p\n", sz_unc, outp);
+  outp = malloc(ubufsize + 4);
+  memcpy(outp + ubufsize, "DCBA", 4);
+#if 0  /* This makes M_LZMA segfault. */
+  ubufsize = 0;
+#endif
+  fprintf(stderr, "outs0=%d outp=%p\n", ubufsize, outp);
   smart_decompress(outp);
-  fprintf(stderr, "canary_ok2=%d\n", 0 == memcmp(outp + sz_unc, "DCBA", 4));
-  /* uncompressed_data_size <= sz_unc. There is padding afterwards. */
-  write(1, before_signature_data, AFTER_SIGNATURE_OFS);
-  write(1, outp, uncompressed_data_size - AFTER_SIGNATURE_OFS);
+  fprintf(stderr, "outs2=%d canary_ok2=%d\n", ubufsize, 0 == memcmp(outp + ubufsize, "DCBA", 4));
+  /* uncompressed_data_size <= ubufsize. There is padding afterwards. */
+  write(1, prefix_data, prefix_data_end - prefix_data);
+  write(1, outp, uncompressed_data_size);
   return 0;
 }
